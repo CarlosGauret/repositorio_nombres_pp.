c92e5dfaa0638edfad27a4288933208aec662a8f
USER <USER@192.168.56.1>
2025-02-12 21:42:56 -0500

    carga incial

---
+++ src/programa02.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class programa02 {
+    public static void main(String[] args) {
+        // Declaración de variables
+        String empleado;
+        double ht, th, sb, d, sn;
+
+        // Creando el objeto lectura
+        Scanner lectura = new Scanner(System.in);
+
+        // Entrada de datos
+        System.out.print("Nombre de empleado: ");
+        empleado = lectura.nextLine(); // Usar nextLine() para capturar nombres completos
+
+        System.out.print("Horas trabajadas: ");
+        ht = lectura.nextDouble();
+
+        System.out.print("Tarifa por hora: ");
+        th = lectura.nextDouble();
+
+        // Proceso de datos
+        sb = ht * th; // Inicializando correctamente sb
+        d = sb * 0.13;
+        sn = sb - d;
+
+        // Salida de datos
+        System.out.println("Sueldo bruto: " + sb);
+        System.out.println("Descuento: " + d);
+        System.out.println("Sueldo Neto: " + sn);
+
+        // Cerrar Scanner
+        lectura.close();
+    }
+}
